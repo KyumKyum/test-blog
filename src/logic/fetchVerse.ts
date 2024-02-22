3bab78e0e24f2f31d83c1f1c7a860df7640fd65a
KyumKyum <mycheesepasta@gmail.com>
2024-02-22 22:46:11 +0900

    Added ENV

--- src/logic/fetchVerse.ts
+++ src/logic/fetchVerse.ts
@@ -2,13 +2,14 @@ import axios from "axios";
 
 //async function fetchVerse(){}
 
+
 const fetchVerse = async (book:string): Promise<string> => {
     const url = 'https://api.esv.org/v3/passage/text/?q=' + book;
     
     try {
         const res = await axios.get(url, {
             headers: {
-                'Authorization': 'Token e04f8b87f8a98e90d1b52f1bd8fe09cac8a78c2d'
+                'Authorization': process.env.NEXT_PUBLIC_API_TOKEN,
             }
         })
 
